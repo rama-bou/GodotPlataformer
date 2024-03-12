94091eaaf981a641345d04769b61ad85ef0668b4
rama-bou <maxiramiro2017@gmail.com>
2024-03-12 17:05:35 -0300

    intento de physics material

---
+++ floor.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	PhysicsMaterial.bounce = 1.0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
